f66b1de840d6de3e86a9619cdbfdf697be15cef0
Red Artist <redartofcode@gmail.com>
2024-01-16 00:32:38 +0530

    print env vars

--- ci.py
+++ ci.py
@@ -5,16 +5,3 @@ print("platform: ", platform.system())
 print("printing env vars:")
 for key, value in os.environ.items():
     print("    ", key, value)
-
-if platform.system() == "Windows" :
-    llvm_path = "C:\\Program Files\\LLVM"
-    if os.path.isdir(llvm_path):
-        print(llvm_path, " exists")
-    else:
-        print(llvm_path, " doesn't exist")
-    os.system("choco install ninja")
-    os.system("ninja --version")
-
-os.system("clang++ --version")
-
-
