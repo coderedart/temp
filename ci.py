f76a4ab035d5aea27b42a6f3e9bc6aabea90d096
Red Artist <redartofcode@gmail.com>
2024-01-19 17:30:54 +0530

    check how matrix works

--- ci.py
+++ ci.py
@@ -5,6 +5,7 @@ print("platform: ", platform.system())
 print("printing env vars:")
 for key, value in sorted(os.environ.items()):
     print("    ", key, value)
+if os.environ['RUNNER_OS'] == 'Linux':
+    file_list = os.listdir(r"/usr/local/lib/android/sdk/")
+    print(file_list)
 
-file_list = os.listdir(r"/usr/local/lib/android/sdk/")
-print(file_list)
